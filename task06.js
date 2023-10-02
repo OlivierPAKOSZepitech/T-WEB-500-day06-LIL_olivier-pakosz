df73a5cc5e337cc24645d263a64aad96dad3a2c0
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-02 15:08:30 +0200

    correction task06

--- task06.js
+++ task06.js
@@ -1,9 +1,22 @@
 export function objectsDeeplyEqual(cmp1, cmp2) {
-    if (cmp1 === cmp2)
-        return true
-    else if (cmp1.length != cmp2.length)
-        return false
-            
-    else if (typeof cmp1 != typeof cmp2)
-        return false
+  if (cmp1 === cmp2) return true;
+  else if (cmp1.length != cmp2.length) {
+    return false;
+  } else if (
+    typeof cmp1 != "object" ||
+    typeof cmp2 != "object" ||
+    typeof cmp1 == null ||
+    cmp2 == null
+  ) {
+    return false;
+  }
+  const keys1 = Object.keys(cmp1);
+  const keys2 = Object.keys(cmp2);
+
+  for (const element of keys1) {
+    if (!objectsDeeplyEqual(cmp1[element], cmp2[element])) {
+      return false;
+    }
+  }
+  return true;
 }
