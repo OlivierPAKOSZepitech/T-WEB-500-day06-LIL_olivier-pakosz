f555f7eebfbea2a56909b055bcee6f545aca897d
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-02 14:05:58 +0200

    correction task01

--- task01.js
+++ task01.js
@@ -1,6 +1,5 @@
 export function drawTriangle(height) {
-    for(let i = 0; i <= height; i++) {
-        console.log("$".repeat(i))
-    }
+  for (let i = 1; i <= height; i++) {
+    console.log("$".repeat(i));
+  }
 }
-
---
+++ task05.js
@@ -0,0 +1,15 @@
+function range(start, end, step) {
+  var array = [];
+  if (step > 0) {
+    for (var i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  } else if (step < 0) {
+    for (var i = start; i >= end; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+
+module.exports = range;
