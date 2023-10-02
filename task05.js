fa9a107d9c5f8c94b5c047f2e3f94397a4185737
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-02 16:30:44 +0200

    Correction task 5

--- task05.js
+++ task05.js
@@ -1,5 +1,8 @@
 function range(start, end, step) {
   var array = [];
+  if (step == undefined) {
+    step = 1
+  }
   if (step > 0) {
     for (var i = start; i <= end; i += step) {
       array.push(i);
