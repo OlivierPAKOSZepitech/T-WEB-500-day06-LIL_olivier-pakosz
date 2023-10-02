53fe58b03eb98147b5c0d17df0a71b03828d2ecf
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-02 12:25:07 +0200

    ajout exo 4

---
+++ task04.js
@@ -0,0 +1,16 @@
+export function fizzBuzz(num){
+    let result = []
+    for(let i = 1; i <= num; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            result.push("FizzBuzz")
+        } else if (i % 5 == 0) {
+            result.push("Buzz")
+        } else if (i % 3 == 0) {
+            result.push("Fizz")
+        } else {
+            result.push(i)
+        }
+        
+    }
+    console.log(result.join(', '))
+}
